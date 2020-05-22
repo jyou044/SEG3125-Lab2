306850053ff7f88bc414475eb03e44a1c5c0de69
Jason You <43392863+jyou044@users.noreply.github.com>
2020-05-22 10:03:10 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,27 @@
-// This function is called when any of the tab is clicked
+/*
+The main.js file.
+This file contains the main underlying javascript used in the website functionality. 
+The code for this file was inspired by 
+https://github.com/carolinebarriere/carolinebarriere.github.io/blob/master/SEG3125-Module2-Grocery/scripts/main.js
+*/
+
+//The openInfo function. This function is called when the tab is clicked by the user. 
 // It is adapted from https://www.w3schools.com/howto/howto_js_tabs.asp
 
 function openInfo(evt, tabName) {
 
-	// Get all elements with class="tabcontent" and hide them
 	tabcontent = document.getElementsByClassName("tabcontent");
 	for (i = 0; i < tabcontent.length; i++) {
 		tabcontent[i].style.display = "none";
 	}
 
-	// Get all elements with class="tablinks" and remove the class "active"
+
 	tablinks = document.getElementsByClassName("tablinks");
 	for (i = 0; i < tablinks.length; i++) {
 		tablinks[i].className = tablinks[i].className.replace(" active", "");
 	}
 
-	// Show the current tab, and add an "active" class to the button that opened the tab
+
 	document.getElementById(tabName).style.display = "block";
 	evt.currentTarget.className += " active";
 
@@ -23,22 +29,18 @@ function openInfo(evt, tabName) {
 
 
 	
-// generate a checkbox list from a list of products
-// it makes each product name as the label for the checkbos
+// The populateListProductChoices function. This is used to create a checkbox list based on the user preference of products. 
 
 function populateListProductChoices(slct1, slct2) {
     var s1 = document.getElementById(slct1);
     var s2 = document.getElementById(slct2);
 	
-	// s2 represents the <div> in the Products tab, which shows the product list, so we first set it empty
+	
     s2.innerHTML = "";
 		
-	// obtain a reduced list of products based on restrictions
+	
     var optionArray = restrictListProducts(products, s1.value);
 
-	// for each item in the array, create a checkbox element, each containing information such as:
-	// <input type="checkbox" name="product" value="Bread">
-	// <label for="Bread">Bread/label><br>
 		
 	for (i = 0; i < optionArray.length; i++) {
 			
@@ -73,7 +75,7 @@ function selectedItems(){
 	var c = document.getElementById('displayCart');
 	c.innerHTML = "";
 	
-	// build list of selected item
+	//Build a list of selected items
 	var para = document.createElement("P");
 	para.innerHTML = "You selected : ";
 	para.appendChild(document.createElement("br"));
@@ -85,7 +87,7 @@ function selectedItems(){
 		}
 	}
 		
-	// add paragraph and total price
+	// Add the paragraph and price number at the bottom of the Page
 	c.appendChild(para);
 	c.appendChild(document.createTextNode("Total Price is $" + getTotalPrice(chosenProducts).toFixed(2)));
 }
